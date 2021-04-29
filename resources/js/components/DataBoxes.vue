e1ea7ed28d9bd1387246fbbebd84ab5e6ed3f86a
bakir1 <bakir_cutuk@hotmail.com>
2021-04-29 18:35:25 +0200

    update 4.0

---
+++ resources/js/components/DataBoxes.vue
@@ -0,0 +1,67 @@
+<template>
+
+<!-- prva kartica -->
+<div class="row">
+<div class="col-xl-3 col-md-6">
+        <div class="card bg-primary text-white mb-4">
+          <div class="card-body">Pozitivni</div>
+          <div
+            class="card-footer d-flex align-items-center justify-content-between"
+          >
+            <a class="medium text-white stretched-link" href="#">Novi: {{ numberWithCommas(stats.NewConfirmed) }}</a>
+            <a class="medium text-white stretched-link" href="/#/statistika">Ukupno: {{ numberWithCommas(stats.TotalConfirmed) }}</a>
+            
+            <div class="medium text-white">
+              <i class="fas fa-angle-right"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-xl-3 col-md-6">
+        <div class="card bg-danger text-white mb-4">
+          <div class="card-body">Umrli</div>
+          <div
+            class="card-footer d-flex align-items-center justify-content-between"
+          >
+            <a class="medium text-white stretched-link" href="#">Novi: {{ numberWithCommas(stats.NewDeaths) }}</a>
+            <a class="medium text-white stretched-link" href="/#/statistika">Ukupno: {{ numberWithCommas(stats.TotalDeaths) }}</a>
+            <div class="medium text-white">
+              <i class="fas fa-angle-right"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-xl-3 col-md-6">
+        <div class="card bg-success text-white mb-4">
+          <div class="card-body">Oporavljeni</div>
+          <div
+            class="card-footer d-flex align-items-center justify-content-between"
+          >
+            <a class="medium text-white stretched-link" href="#">Novi: {{ numberWithCommas(stats.NewRecovered) }}</a>
+            <a class="medium text-white stretched-link" href="/#/statistika">Ukupno: {{ numberWithCommas(stats.TotalRecovered) }}</a>
+            <div class="small text-white">
+              <i class="fas fa-angle-right"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'DataBoxes',
+    props: ['stats'],
+    methods: {
+        numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        }
+    }
+}
+</script>
--- resources/js/views/Welcome.vue
+++ resources/js/views/Welcome.vue
@@ -7,7 +7,7 @@
       </li>
       <li class="breadcrumb-item active">Prikaz</li>
     </ol>
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-xl-3 col-md-6">
         <div class="card bg-primary text-white mb-4">
           <div class="card-body">Primary Card</div>
@@ -60,9 +60,71 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
+    <main v-if="!loading">
+      <DataTitle :text="title" :dataDate="dataDate" />
+      <DataBoxes :stats="stats" />
+      <CountrySelect @get-country="getCountryData" :countries="countries" />
 
+      <b-button @click="clearCountryData" v-if="stats.Country" variant="outline-primary"
+        >Novi Podaci</b-button
+      >
+    </main>
 
+    <main class="flex flex-col align-center justify-center text-center" v-else>
+      <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    </main>
   </div>
 </template>
+
+<script>
+import DataTitle from "../components/DataTitle.vue";
+import DataBoxes from "../components/DataBoxes.vue";
+import CountrySelect from "../components/CountrySelect.vue";
+
+export default {
+  name: "pocetna",
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      status: {},
+      countries: [],
+      //      loadingImage: require('')
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData(){
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false 
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
+</script>
